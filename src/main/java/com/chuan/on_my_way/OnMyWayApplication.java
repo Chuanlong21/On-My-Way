8778e207a1d2f7f5a5886e834e927ea019ac33fc
chuan <chuanlon@buffalo.edu>
2022-11-18 19:27:13 -0500

    Initial

---
+++ src/main/java/com/chuan/on_my_way/OnMyWayApplication.java
@@ -0,0 +1,16 @@
+package com.chuan.on_my_way;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+@Slf4j
+public class OnMyWayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnMyWayApplication.class, args);
+    }
+
+}
